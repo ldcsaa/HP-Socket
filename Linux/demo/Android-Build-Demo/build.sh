fe9497588eb471910ea24107b349c78e77e7bdb8
liangdongcheng <liangdongcheng@lechebang.com>
2018-11-01 09:45:14 +0800

    20181101

--- Linux/demo/Android-Build-Demo/build.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-export PATH=/usr/aarch64-linux-android/bin/:$PATH
-export PATH=/usr/arm-linux-android/bin/:$PATH
-
-CXX=arm-linux-androideabi-g++ AR=arm-linux-androideabi-ar make
-mv build build32
-CXX=aarch64-linux-android-g++ AR=aarch64-linux-android-ar make
-mv build build64
-
