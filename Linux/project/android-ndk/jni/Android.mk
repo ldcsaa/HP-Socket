bf4e42a7fcc1692d3a885d5e725fd30246555127
ldcsaa <ldcsaa@21cn.com>
2019-04-14 07:51:37 +0800

    20190414

--- Linux/project/android-ndk/jni/Android.mk
+++ Linux/project/android-ndk/jni/Android.mk
@@ -1,44 +1,44 @@
 LOCAL_PATH   := $(call my-dir)
 
 MY_SRC_FILES := ../../../src/common/BufferPool.cpp \
-					../../../src/common/crypto/Crypto.cpp \
-					../../../src/common/Event.cpp \
-					../../../src/common/FileHelper.cpp \
-					../../../src/common/FuncHelper.cpp \
-					../../../src/common/http/http_parser.c \
-					../../../src/common/IODispatcher.cpp \
-					../../../src/common/kcp/ikcp.c \
-					../../../src/common/PollHelper.cpp \
-					../../../src/common/RWLock.cpp \
-					../../../src/common/SysHelper.cpp \
-					../../../src/common/Thread.cpp \
-					../../../src/ArqHelper.cpp \
-					../../../src/HPThreadPool.cpp \
-					../../../src/HttpAgent.cpp \
-					../../../src/HttpClient.cpp \
-					../../../src/HttpCookie.cpp \
-					../../../src/HttpHelper.cpp \
-					../../../src/HttpServer.cpp \
-					../../../src/MiscHelper.cpp \
-					../../../src/SocketHelper.cpp \
-					../../../src/SSLAgent.cpp \
-					../../../src/SSLClient.cpp \
-					../../../src/SSLHelper.cpp \
-					../../../src/SSLServer.cpp \
-					../../../src/TcpAgent.cpp \
-					../../../src/TcpClient.cpp \
-					../../../src/TcpPackAgent.cpp \
-					../../../src/TcpPackClient.cpp \
-					../../../src/TcpPackServer.cpp \
-					../../../src/TcpPullAgent.cpp \
-					../../../src/TcpPullClient.cpp \
-					../../../src/TcpPullServer.cpp \
-					../../../src/TcpServer.cpp \
-					../../../src/UdpArqClient.cpp \
-					../../../src/UdpArqServer.cpp \
-					../../../src/UdpCast.cpp \
-					../../../src/UdpClient.cpp \
-					../../../src/UdpServer.cpp
+                ../../../src/common/crypto/Crypto.cpp \
+                ../../../src/common/Event.cpp \
+                ../../../src/common/FileHelper.cpp \
+                ../../../src/common/FuncHelper.cpp \
+                ../../../src/common/http/http_parser.c \
+                ../../../src/common/IODispatcher.cpp \
+                ../../../src/common/kcp/ikcp.c \
+                ../../../src/common/PollHelper.cpp \
+                ../../../src/common/RWLock.cpp \
+                ../../../src/common/SysHelper.cpp \
+                ../../../src/common/Thread.cpp \
+                ../../../src/ArqHelper.cpp \
+                ../../../src/HPThreadPool.cpp \
+                ../../../src/HttpAgent.cpp \
+                ../../../src/HttpClient.cpp \
+                ../../../src/HttpCookie.cpp \
+                ../../../src/HttpHelper.cpp \
+                ../../../src/HttpServer.cpp \
+                ../../../src/MiscHelper.cpp \
+                ../../../src/SocketHelper.cpp \
+                ../../../src/SSLAgent.cpp \
+                ../../../src/SSLClient.cpp \
+                ../../../src/SSLHelper.cpp \
+                ../../../src/SSLServer.cpp \
+                ../../../src/TcpAgent.cpp \
+                ../../../src/TcpClient.cpp \
+                ../../../src/TcpPackAgent.cpp \
+                ../../../src/TcpPackClient.cpp \
+                ../../../src/TcpPackServer.cpp \
+                ../../../src/TcpPullAgent.cpp \
+                ../../../src/TcpPullClient.cpp \
+                ../../../src/TcpPullServer.cpp \
+                ../../../src/TcpServer.cpp \
+                ../../../src/UdpArqClient.cpp \
+                ../../../src/UdpArqServer.cpp \
+                ../../../src/UdpCast.cpp \
+                ../../../src/UdpClient.cpp \
+                ../../../src/UdpServer.cpp
 MY_C_INCLUDES := $(LOCAL_PATH)/../../../src
 MY_CFLAGS     := -mfloat-abi=softfp -fpic -fvisibility=hidden -fno-strict-aliasing -Wall -Wextra -Wswitch -Wno-deprecated-declarations -Wno-empty-body -Wno-conversion -Wno-sign-conversion -Wno-sign-compare -Wreturn-type -Wparentheses -Wno-pointer-sign -Wno-format -Wno-missing-braces -Wuninitialized -Wunreachable-code -Wunused-function -Wunused-value -Wunused-variable -Wno-unused-parameter -Wno-missing-field-initializers
 MY_CPPFLAGS   := -fexceptions -frtti -fthreadsafe-statics -Wno-reorder -Wno-inconsistent-missing-override
