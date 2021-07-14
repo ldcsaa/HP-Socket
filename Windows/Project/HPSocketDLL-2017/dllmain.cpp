e0e61d1fa5d57ff00da1148483370b13a65ef5cc
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-14 20:43:29 +0800

    Delete Windows directory

--- Windows/Project/HPSocketDLL-2017/dllmain.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	return TRUE;
-}
--- Windows/copy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-xcopy /y /s /f Bin\HPSocket\x86\HPSocket_U.dll Demo\Release\x86\
-xcopy /y /s /f Bin\HPSocket\x64\HPSocket_U.dll Demo\Release\x64\
-
-xcopy /y /s /f Bin\HPSocket4C\x86\HPSocket4C_U.dll Demo\Release\x86\
-xcopy /y /s /f Bin\HPSocket4C\x64\HPSocket4C_U.dll Demo\Release\x64\
