2cd9788074a8e05b6076ef4cb3c573c257008c19
ldcsaa <ldcsaa@21cn.com>
2024-09-21 04:29:37 +0800

    20240921

--- Windows/Src/Common/http/llhttp.h
+++ Windows/Src/Common/http/llhttp.h
@@ -550,8 +550,8 @@ extern "C" {
 
 #if defined(__wasm__)
 #define LLHTTP_EXPORT __attribute__((visibility("default")))
-#elif defined(_WIN32)
-#define LLHTTP_EXPORT __declspec(dllexport)
+//#elif defined(_WIN32)
+//#define LLHTTP_EXPORT __declspec(dllexport)
 #else
 #define LLHTTP_EXPORT
 #endif
